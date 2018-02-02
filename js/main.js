5c70c67185b9d19d763807ddef22088a1d962a88
Teilachanell Angel <angel@Teilachanells-MacBook-Pro.local>
2018-02-02 18:20:09 -0500

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,11 +1,29 @@
 
 console.log("Up and running!");
 
-var cardOne = "Queen";
-var cardTwo = "Queen";
-var cardThree = "King";
-var cardFour = "King";
 
 
-console.log("User flipped " + cardOne);
-console.log("User flipped " + cardThree);
+
+//console.log("User flipped " + cardOne);
+//console.log("User flipped " + cardThree);
+
+var cards =["queen", "queen", "king", "king"];
+var cardOne = cards[0];
+var cardTwo = cards[2];
+var cardsInPlay= [];
+
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("The user flipped " + cardOne);
+console.log("The user flipped " + cardTwo);
+
+if(cardsInPlay.length === 2){
+	if(cardsInPlay[0] === cardsInPlay[1]){
+		alert("You found a match!");
+	}else{
+		alert("Sorry, try again.");
+	
+	}
+}
+

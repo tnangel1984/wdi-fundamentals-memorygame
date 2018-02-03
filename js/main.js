182872a4b089ccbfe029d0e19cb1b93187a6de71
Teilachanell Angel <angel@Teilachanells-MacBook-Pro.local>
2018-02-03 18:01:49 -0500

    created game logic again

--- js/main.js
+++ js/main.js
@@ -4,26 +4,31 @@ console.log("Up and running!");
 
 
 
-//console.log("User flipped " + cardOne);
-//console.log("User flipped " + cardThree);
-
+// arrays for card options and selected cards.
 var cards =["queen", "queen", "king", "king"];
-var cardOne = cards[0];
-var cardTwo = cards[2];
 var cardsInPlay= [];
 
-cardsInPlay.push(cardOne);
-cardsInPlay.push(cardTwo);
 
-console.log("The user flipped " + cardOne);
-console.log("The user flipped " + cardTwo);
-
-if(cardsInPlay.length === 2){
-	if(cardsInPlay[0] === cardsInPlay[1]){
-		alert("You found a match!");
-	}else{
-		alert("Sorry, try again.");
-	
+//Once selected cards have been added to the array, will check for match.
+function checkForMatch(){
+	if(cardsInPlay.length === 2){
+		if(cardsInPlay[0] === cardsInPlay[1]){
+			alert("You found a match!");
+		}else{
+			alert("Sorry, try again.");
+		
+		}
 	}
+
 }
 
+//returns card selection, adds selection to array InPlay, calls check for match funciton above.
+function flipCard (cardId){
+	console.log("The user flipped " +  cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+	console.log(checkForMatch());
+}
+
+
+console.log(flipCard(0));
+console.log(flipCard(1));

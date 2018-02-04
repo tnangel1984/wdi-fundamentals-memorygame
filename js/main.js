3e3a90ac283693c8d9fb78b2c049ad9881611a9c
Teilachanell Angel <angel@Teilachanells-MacBook-Pro.local>
2018-02-03 21:02:36 -0500

    created game logic again

--- js/main.js
+++ js/main.js
@@ -1,11 +1,38 @@
 
-console.log("Up and running!");
+//console.log("Up and running!");
 
 
 
 
 // arrays for card options and selected cards.
-var cards =["queen", "queen", "king", "king"];
+var cards =[
+  {
+  	rank: "queen",
+  	suit: "hearts",
+  	cardImage: "images/queen-of-hearts.png"
+  },
+
+  {
+  	rank: "queen",
+  	suit: "diamonds",
+  	cardImage: "images/queen-of-diamonds.png"
+  },
+
+  {
+  	rank: "king",
+  	suit: "hearts",
+  	cardImage:"images/king-of-hearts.png"
+  },
+
+  {
+  	rank: "king",
+  	suit: "diamonds",
+  	cardImage: "images/king-of-diamonds.png"
+  }
+
+];
+
+
 var cardsInPlay= [];
 
 
@@ -24,11 +51,13 @@ function checkForMatch(){
 
 //returns card selection, adds selection to array InPlay, calls check for match funciton above.
 function flipCard (cardId){
-	console.log("The user flipped " +  cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("The user flipped " +  cards[cardId].rank);
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
+	cardsInPlay.push(cards[cardId].rank);
 	console.log(checkForMatch());
 }
 
 
 console.log(flipCard(0));
-console.log(flipCard(1));
+console.log(flipCard(2));
